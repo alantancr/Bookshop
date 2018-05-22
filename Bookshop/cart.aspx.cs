1b8e3e41ee40f33a7a872a5afc3be25176b50f49
Alan Tan <36091630+alantancr@users.noreply.github.com>
2018-05-22 20:35:45 +0800

    logincommit3

--- Bookshop/cart.aspx.cs
+++ Bookshop/cart.aspx.cs
@@ -11,8 +11,7 @@ namespace Bookshop
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string url = HttpContext.Current.Request.Url.AbsolutePath;
-            Session["url"] = url;
+           
 
         }
 
