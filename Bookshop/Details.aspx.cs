688ec78cf33ac573baa7d5bde6c63566278a2000
e0283993 <e0283993@ISS-BOR21103>
2018-05-22 15:10:26 +0800

    Test commit

--- Bookshop/Details.aspx.cs
+++ Bookshop/Details.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Bookshop
 {
+    // Test comment
     public partial class Details : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
